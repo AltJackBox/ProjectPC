0c008153daf1fdc18acdcf5e15612b7467f02e16
Yaël <ya38able@gmail.com>
2020-12-06 16:37:19 +0100

    Fichiers source

---
+++ src/prodcons/v1/TestProdCons.java
@@ -0,0 +1,13 @@
+package prodcons.v1;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class TestProdCons {
+
+	Properties properties = new Properties();
+	FileInputStream file = new FileInputStream("options.xml");
+//	int nProd = Integer.parseInt(properties.getProperty(“nProc”));
+//	int nCons = Integer.parseInt(properties.getProperty(“nCons”));
+}
---
+++ src/utils/IProdConsBuffer.java
@@ -0,0 +1,24 @@
+package utils;
+
+public interface IProdConsBuffer {
+	/**
+	* Put the message m in the prodcons buffer
+	**/
+	public void put(Message m) throws InterruptedException;
+	/**
+	* Retrieve a message from the prodcons buffer,
+	* following a FIFO order (if M1 was put before M2, M&
+	* will be get before M2)
+	**/
+	public Message get() throws InterruptedException;
+	/**
+	* Returns the number of messages currently available in
+	* the prod-cons buffer
+	**/
+	public int nmsg();
+	/**
+	* Returns the total number of messages that have
+	* been put in the buffer since its creation
+	**/
+	public int totmsg();
+}
