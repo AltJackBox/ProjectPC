54ffaa92351dfd8051872e41ad40ec15b561119e
Yaël <ya38able@gmail.com>
2020-12-06 17:02:19 +0100

    Putain de XML

--- src/prodcons/v1/TestProdCons.java
+++ src/prodcons/v1/TestProdCons.java
@@ -1,13 +1,21 @@
 package prodcons.v1;
 
 import java.io.FileInputStream;
-import java.io.InputStream;
+import java.io.IOException;
+import java.util.InvalidPropertiesFormatException;
 import java.util.Properties;
 
 public class TestProdCons {
 
-	Properties properties = new Properties();
-	FileInputStream file = new FileInputStream("options.xml");
-//	int nProd = Integer.parseInt(properties.getProperty(“nProc”));
-//	int nCons = Integer.parseInt(properties.getProperty(“nCons”));
+	public static void main(String[] args) throws InvalidPropertiesFormatException, IOException {
+		Properties properties = new Properties();
+		properties.loadFromXML(new FileInputStream("options.xml"));
+		int nProd = Integer.parseInt(properties.getProperty("nProd"));
+		int nCons = Integer.parseInt(properties.getProperty("nCons"));
+		int bufSz = Integer.parseInt(properties.getProperty("bufSz"));
+		int prodTime = Integer.parseInt(properties.getProperty("prodTime"));
+		int consTime = Integer.parseInt(properties.getProperty("consTime"));
+		int minProd = Integer.parseInt(properties.getProperty("minProd"));
+		int maxProd = Integer.parseInt(properties.getProperty("maxProd"));
+	}
 }
